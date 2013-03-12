1e51dd749d139fb2480fadfc276594341a6c43d8
Nils Ekman <nisse.ekman@gmail.com>
2013-03-13 00:13:23 +0100

    Fixed dublicate menuitem error. Don't create menus with undefined id

--- src/jsview.js
+++ src/jsview.js
@@ -1,15 +1,15 @@
+/*
+ * TODO: Refactor, test and performance!
+ */
 (function() {
-
     var MENU_TEMPLATE = 'JS ({JS}) CSS ({CSS})',
         PARENT_ID = 'main',
         TYPE_JS = 'JS',
         TYPE_CSS = 'CSS',
         menus = chrome.contextMenus,
-        tabs = chrome.tabs;
+        tabs = chrome.tabs,
 
     executeContentScript = function(tabId) {
-      menus.removeAll();
-
       tabs.get(tabId, function(tab) {
         if (/^(http|https):\/\//.test(tab.url)) {
           tabs.executeScript(tabId, {
@@ -23,6 +23,7 @@
       menus.create({
         parentId: PARENT_ID,
         title: title,
+        id: title,
         enabled: false
       });
     },
@@ -31,7 +32,7 @@
       files.forEach(function(url) {
         menus.create({
           parentId: PARENT_ID,
-          title: url.split('/').pop(),
+          title: url.split('/').pop() || url,
           id: url
         });
       });
@@ -40,31 +41,36 @@
     createMenuSection = function(files, type) {
       createHeaderItem(type);
       createMenuItems(files);
-
-      if (type === TYPE_JS) {
-        // Add separator
-        menus.create({
-          parentId: PARENT_ID,
-          type: 'separator'
-        });
-      }
     },
 
     handleOnMessage = function(message, sender, sendResponse) {
+      menus.removeAll();
+
       var urls = JSON.parse(message),
-          totalJSFiles = urls.jsFiles.length,
-          totalCSSFiles = urls.cssFiles.length;
+          numberOfJSFiles = urls.jsFiles.length,
+          numberOfCSSFiles = urls.cssFiles.length,
+          hasJsFiles = numberOfJSFiles > 0,
+          hasCssFiles = numberOfCSSFiles > 0;
 
       // Create main menu
       menus.create({
         id: PARENT_ID,
         title: MENU_TEMPLATE
-                .replace(/{JS}/, totalJSFiles)
-                .replace(/{CSS}/, totalCSSFiles)
+                .replace(/{JS}/, numberOfJSFiles)
+                .replace(/{CSS}/, numberOfCSSFiles)
       });
 
-      totalJSFiles && createMenuSection(urls.jsFiles, TYPE_JS);
-      totalCSSFiles &&  createMenuSection(urls.cssFiles, TYPE_CSS);
+      hasJsFiles && createMenuSection(urls.jsFiles, TYPE_JS);
+
+      if (hasCssFiles) {
+        // Show separator if JS-files section is created.
+        hasJsFiles && menus.create({
+          parentId: PARENT_ID,
+          type: 'separator'
+        });
+
+        createMenuSection(urls.cssFiles, TYPE_CSS);
+      }
     },
 
     handleOnContextMenuClick = function(info, tab) {
@@ -72,12 +78,9 @@
     },
 
     handleOnTabUpdated = function(tabId, changeInfo, tab) {
+      // Need to wait until the tab is complete.
       if (changeInfo.status === 'complete') {
-          // Need to wait here, so that the activated tab can complete.
-          // TODO: Fix.
-          setTimeout(function() {
-            executeContentScript(tabId);
-          }, 200);
+          executeContentScript(tabId);
       }
     },
 
